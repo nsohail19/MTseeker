e0fa78783db211fafcf206e6dbbb479fa0938d39
nsohail19 <koheenur@gmail.com>
2019-08-09 15:21:09 -0400

    filter up with input from user

---
+++ R/rawMTreads.R
@@ -0,0 +1,37 @@
+#' Raw reads shown as GAlignments[List]
+#'
+#' @param bams    List of files
+#'
+#' @return        GAlignments or GAlignmentsList of raw reads
+#'
+#'
+#' @examples 
+#' 
+#' library(MTseekerData)
+#' BAMdir <- system.file("extdata", "BAMs", package="MTseekerData")
+#' BAMs <- list.files(BAMdir, pattern="bam$")
+#' BAMs <- paste0(BAMdir, "/", BAMs)
+#' 
+#' rawReads <- rawMTreads(BAMs)
+#' 
+#' rawReads_1 <- MAlignments(rawReads[[1]], BAMs[1])
+#' plotMTCoverage(rawReads_1)
+#'
+
+rawMTreads <- function(bams) {
+  
+  # To return a list
+  if (length(bams) > 1) {
+    
+    manyBams <- lapply(bams, rawMTreads)
+    return(GAlignmentsList(manyBams))
+  }
+  
+  else {
+    
+    sbp <- scanMT(bams)
+    bamWhat(sbp) <- "seq"
+    reads <- readGAlignments(file=bams, param=sbp)
+    return(reads)
+  }
+}
